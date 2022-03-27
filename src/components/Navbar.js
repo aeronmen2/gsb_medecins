a20cc00a56abb73cddc03b38faba5c506d9fd0e9
aeronmen2 <75632261+aeronmen2@users.noreply.github.com>
2022-03-27 20:21:06 +0200

    new changes, nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,54 @@
-import React from 'react';
-import {
-  Nav,
-  NavLink,
-  Bars,
-  NavMenu,
-  NavBtn,
-  NavBtnLink
-} from './NavbarElements';
+import React  from "react";
+import { Link, NavLink } from "react-router-dom";
+import styled from "styled-components";
+import logo from "./logo.png";
 
-const Navbar = () => {
-  return (
-    <>
-      <Nav>
-        <NavLink to='/'>
-        </NavLink>
-        <Bars />
-        <NavMenu>
-          <NavLink to='/medecins' activeStyle>
-            Médecins
-          </NavLink>
-          <NavLink to='/departements' activeStyle>
-            Départements
-          </NavLink>
-          <NavLink to='/pays' activeStyle>
-            Pays
-          </NavLink>
-        </NavMenu>
-      </Nav>
-    </>
-  );
-};
+
+const NavUnlisted = styled.ul`
+  
+  display: flex;
+  a {
+    text-decoration: none;
+  }
+  li {
+    text-align: center;
+    color: red;
+    margin: 0 0.8rem;
+    font-size: 1.3rem;
+    position: relative;
+    list-style: none;
+  }
+
+  .current {
+    li {
+      border-bottom: 2px solid black;
+    }
+  }
+`;
+
+
+const links = [
+    {name: "Accueil",path:"/"},
+    {name: "Médecins",path:"/medecins"},
+    {name: "Départements",path:"/departements"},
+    {name: "Pays",path:"/pays"},
+];
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <img src={logo} alt="Logo" width={270}/>
+                <NavUnlisted>
+                    {links.map((link,index) => (
+                        <NavLink key={index} to={link.path} exact activeClassName="current">
+                            <li>{link.name}</li>
+                        </NavLink>
+                    ))}
+                </NavUnlisted>
+        </div>
+    );
+}
+
+
 
 export default Navbar;
